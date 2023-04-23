d722672889081f9c98d47a4d99bd3eb76d2ad153
szkkb <davidzhou99499@gmail.com>
2023-04-23 23:12:48 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
-  { text: "主页", icon: "blog", link: "/blog" },
+  { text: "介绍", icon: "blog", link: "/blog" },
   {
     text: "博客",
     icon: "code",
